a8b861b5c44dedd8bcd67172545616518f9a6e24
kentatogashi <sufla2013@gmail.com>
2012-07-22 00:04:25 +0900

    testing

--- test.php
+++ test.php
@@ -1,19 +1,2 @@
 <?php
-/*
-$dead_day = date("Y-m-d",time()+($due_day * 24 * 60 * 60));//設定日時
-$now = time();
-classに入れる。
-contentsの中のすべてのユーザに対して、
-ユーザの名前とduedayをget.
-$nowと$deadayを　unixタイムに変換して
-if($now > $deadday){
-	mailアドレスを取得。
-	mailを送信する。
-}
-別のファイルで出力させる。
-*/
-require_once("class/member_db_class");
 
-$obj = new operationDb();
-
-$obj->searchAll(TABLE_CONTENT);
