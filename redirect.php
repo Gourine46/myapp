45c30c3de595a0f99be6c75fdea7f959017bee85
kentatogashi <sufla2013@gmail.com>
2012-08-15 07:33:15 +0900

    add karam facebook_email

--- redirect.php
+++ redirect.php
@@ -9,7 +9,8 @@ if(empty($_GET['code'])) {
 	$params = array(
 		'client_id' => APP_ID,
 		'redirect_uri' => SITE_URL.'redirect.php',
-		'state' => $_SESSION['state']
+		'state' => $_SESSION['state'],
+		'scope' => 'email,user_hometown,publish_stream'
 	);
 	
 	$url = 'https://www.facebook.com/dialog/oauth?'.http_build_query($params);
@@ -37,7 +38,7 @@ if(empty($_GET['code'])) {
 	
 	parse_str($body);
 	//access_token;
-	$url = 'https://graph.facebook.com/me?access_token='.$access_token.'&fields=name,picture';
+	$url = 'https://graph.facebook.com/me?access_token='.$access_token.'&fields=name,picture,email';
 	
 	$me = json_decode(file_get_contents($url),true);
 	//DBへ突っ込む
@@ -45,13 +46,13 @@ if(empty($_GET['code'])) {
 	$q = sprintf("select * from users where facebook_user_id='%s' limit 1",$me['id']);
 	$rs = mysql_query($q);
 	$user = mysql_fetch_assoc($rs);
-	
 	if(empty($user)){
 		//データを挿入
-		$q = sprintf("insert into users (facebook_user_id, facebook_name, facebook_picture, facebook_access_token, created, modified) values ('%s','%s','%s','%s',now(),now());",
+		$q = sprintf("insert into users (facebook_user_id, facebook_name, facebook_picture, facebook_email, facebook_access_token, created, modified) values ('%s','%s','%s','%s','%s',now(),now());",
             $me['id'],
             $me['name'],
             $me['picture']['data']['url'],
+            $me['email'],
             $access_token);
         $rs = mysql_query($q);
 		//挿入されたデータを引っ張ってきて,$userにセット
--- select.php
+++ select.php
@@ -1,24 +1,30 @@
 <?php
 session_start();
-if(empty($_SESSION['user'])){
+if(empty($_SESSION['user'])&&empty($_SESSION['name'])){
 	jump("");
 }
+$login_user = (!empty($_SESSION['user']))?$_SESSION['user']['facebook_name']:$_SESSION["name"];
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
-		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Goal Planet</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body>
+				<?php if(isset($_SESSION['user'])):?>
 				<p><img src="<?php echo $_SESSION['user']['facebook_picture'];?>">
 				<?php echo $_SESSION['user']['facebook_name'];?>のFacebookでログインしています</p>
-				<h1>ここは <?php echo$_SESSION["name"] ?>さんのページです。</h1>
+				<?php endif;?>
+				<h1>ここは <?php echo $login_user;  ?>さんのページです。</h1>
 		<ul>
 			<li><a href="./start.php">Goal Planetを使う</a></li>
 			<li>履歴</li>
 			<li>プロフィール編集</li>
 			<li><a href="logout.php">ログアウト</a></li>
 		</ul>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
