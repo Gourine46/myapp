dba60b72435bef775f58245925a7ccd8fa926577
kentatogashi <sufla2013@gmail.com>
2012-08-16 11:10:10 +0900

    add me.php

---
+++ me.php
@@ -0,0 +1,45 @@
+<?php
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>About me</h1>
+	<p><a href="">facebook</a></p>
+	<p><a href="">Twitter</a></p>
+<div>
+	<h2>所属:</h2>
+		<p>埼玉大学教養学部国際政治学ゼミ,現在4回生</p>
+		<p>裏千家茶道部茶楽会 渉外と他大学との窓口係</p>
+		<p>とあるWebサービスの企業でシステムエンジニアとして勤務</p>
+	<h2>スキル:</h2>
+		<ul>
+			<li>ツイストパーソン</li>
+			<li>ショートスリーパー</li>
+			<li>マイペース</li>
+			<li>ビヨンドエクスペクテーションズ</li>
+			<li>スマイリー</li>
+			<li>ユースフルネス</li>
+			<li>レーショナリズム</li>
+		</ul>
+	<h2>webスキル:</h2>
+		<ul>
+			<li>HTML</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>Git</li>
+			<li>Linux(簡単なコマンドいじるくらい...)</li>
+			<li>その他(wordpressの設定,cakePHP,facebookTwitterのAPI,twitterbootstrapとか少しいじれます。)</li>
+		</ul>
+	<h2>制作物:</h2>	
+		<ul>
+			<li><a href="http://togattti.lolipop.jp/myapp/">GoalPlanet</a></li>
+		</ul>
+		
+</div>
+</body>
+</html>
